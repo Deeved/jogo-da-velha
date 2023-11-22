a2e6956d421c8d2aa903fbcfce1ce4800fce2d87
Deeved Hiuston <deeved@gmail.com>
2023-11-21 21:46:38 -0300

    melhoria

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { winnerPatterns } from './utils';
 
 @Component({
   selector: 'app-game',
@@ -24,17 +25,6 @@ export class GameComponent {
 
   isAllowedPlay = true;
 
-  padroesDeVitorias = [
-    [0, 1, 2],
-    [0, 4, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-  ];
-
   winner: string = '';
 
   selectedPosition(position: number): void {
@@ -56,13 +46,13 @@ export class GameComponent {
   }
 
   checkResult() {
-    for (let padraoVitoria of this.padroesDeVitorias) {
+    for (let winnerPattern of winnerPatterns) {
       if (
-        this.board[padraoVitoria[0]].value &&
-        this.board[padraoVitoria[0]].value ===
-          this.board[padraoVitoria[1]].value &&
-        this.board[padraoVitoria[1]].value ===
-          this.board[padraoVitoria[2]].value
+        this.board[winnerPattern[0]].value &&
+        this.board[winnerPattern[0]].value ===
+          this.board[winnerPattern[1]].value &&
+        this.board[winnerPattern[1]].value ===
+          this.board[winnerPattern[2]].value
       ) {
         console.log('houve um vencedor', this.playerTurner);
         this.isAllowedPlay = false;
